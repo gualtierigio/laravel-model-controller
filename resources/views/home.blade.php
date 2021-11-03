3daba33f610b2c7b7aad0600320da2166ed934ee
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-11-03 17:55:52 +0100

    completo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,15 @@
 
 @section('comtent')
 
-    {{-- <section id="movies" class="container p-5">
+    <section id="movies" class="container p-5">
         @forelse ($movies as $movie)
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">{{ $movie->title}}</h4>
-                    <h6 class="card-subtitle">{{ substr($movie->original_title, 25, 55) }}</h5>
+                    <h6 class="card-subtitle">{{ $movie->original_title }}</h5>
                     <div class="card-text">
                         <p>{{ $movie->nationality}}</p>
-                        {{-- usate <address> --}}
+                    
                         <pre>{{ $movie->vote}}</pre>
                     </div>    
                 </div>
@@ -20,6 +20,6 @@
         @empty
             <h3>Nessun film è disponibbile</h3>
         @endforelse
-    </section> --}}
+    </section>
 
 @endsection
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -7,7 +7,7 @@
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title">{{ $movie->title}}</h4>
-                <h5 class="card-subtitle">{{ substr($movie->original_title, 25, 55) }}</h5>
+                <h5 class="card-subtitle">{{ $movie->original_title}}</h5>
                 <div class="card-text">
                     <p>{{ $movie->nationality}}</p>
                     {{-- usate <address> --}}
